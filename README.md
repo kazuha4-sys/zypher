53f0d173aaae8c8c5d7a87413ea0202fbea94b0e
DEDSEC GAMEPLAYS <00001126341162sp@al.educacao.sp.gov.br>
2025-02-28 15:29:13 -0300

    Dei uma arrumada no README, index, e criei uma pasta para os templates em css

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# zypher
+# Rede Social Inovadora
+
+Este repositório contém o código e os recursos principais de uma **rede social inovadora** focada em **conectar usuários de maneira eficiente** e promover **interações dinâmicas e seguras**. O objetivo da rede social é criar um ambiente onde as pessoas possam compartilhar ideias, se comunicar e colaborar de maneira intuitiva, respeitando a privacidade e a segurança dos dados.
+
+## Estrutura da pasta 
+SocialSec
+├── lib
+│   └── blackjack.ex
+├── mix.exs
+├── README.md
+└── test
+    ├── blackjack_test.exs
+    └── test_helper.exs
+
+
+## Funcionalidades Principais
+
+- **Interface Intuitiva**: A plataforma foi projetada para oferecer uma navegação fácil e agradável, com uma experiência de usuário moderna.
+- **Conexão de Usuários**: Permite aos usuários se conectarem com outras pessoas baseadas em interesses e preferências.
+- **Segurança e Privacidade**: Focada em garantir que os dados dos usuários sejam protegidos por meio de práticas de segurança avançadas.
+- **Sistema de Notificações**: Os usuários recebem atualizações em tempo real sobre as interações e atividades de sua rede.
+- **Interações em Tempo Real**: A plataforma oferece ferramentas para comunicação instantânea, como mensagens e notificações.
+
+## Tecnologias Utilizadas
+
+Este projeto é construído com as tecnologias mais modernas e eficientes, incluindo (aqui você pode mencionar algumas tecnologias gerais sem revelar detalhes cruciais, como frameworks ou integrações específicas).
+
+## Contribuições
+
+Este repositório é aberto a contribuições. Buscamos colaboradores interessados em aprimorar a plataforma, agregar novas funcionalidades e melhorar a segurança.
+
+**Importante**: Para preservar a integridade e a originalidade do projeto, não divulgamos detalhes específicos sobre a estrutura interna do sistema, protocolos de comunicação ou funcionalidades exclusivas implementadas. Estamos comprometidos com a inovação, mas também com a proteção das ideias e tecnologias envolvidas.
+
+## Licença
+
+Este projeto está sob a licença [Nome da Licença], o que significa que você pode usar, modificar e distribuir o código sob as condições estabelecidas na licença.
--- index.php
+++ index.php
@@ -5,3 +5,183 @@ session_start();
 
 
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Watch Dogs Font</title>
+        <link rel="icon" href="favicon.png" />
+        <link rel="stylesheet" href="//dav.li/jquery/ui/jquery-ui.css"/>
+        <link rel="stylesheet" href="templates/css/style.css"/>
+        <link rel="stylesheet" href="templates/css/retro_Hack.css"/>
+        <meta name="keywords" content="Watch Dogs hacked font free download HACKED design youtube art" />
+        <meta name="description" content="Discover and download the free Hacked font, a recreation of the Watch Dogs logo's font." />
+    </head>
+    <body>
+        <header>
+           <div id="headervideo">
+                 <iframe src="https://www.youtube-nocookie.com/embed?listType=playlist&list=PLYB-04VHf1gZZ9TRE0BYzX5XTkofAIJdE&version=3&loop=1&autoplay=1&rel=0&showinfo=0&controls=0&autohide=1"></iframe>
+                <div>
+                    <span>a</span><span>b</span><span>c</span><span>d</span>
+                    <img src="img/LIGHTNING.png" alt="LIGHTING" />
+                </div>
+           </div>
+           <h2>Be ready to be <strong>hacked</strong></h2>
+           <h1>watch_dogs font</h1>            
+        </header>
+        
+        <main>
+            <div id="whiteborder"></div>
+            <svg version="1.1" id="svg_v" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" enable-background="new 0 0 841.89 204.28" xml:space="preserve">
+                <g>
+	                <polygon points="841.89,204.28 0,204.28 0,0 420.945,127.669 841.89,0"/>
+                </g>
+            </svg>
+
+            <article>
+                <h2>Discover the font</h2>
+                <p><img src="img/watch_dogs_font_hacked_overview.jpg" alt="Hacked font overview"/></p>
+                <form id="testfontform">
+                    <input type="text" placeholder="Type here to try"/>
+                </form>
+                <pre id="console"></pre>
+            </article>
+
+            <article>
+                <h2>Explore the font</h2>
+                <div>
+                    <span id="ghost_vr"></span>
+                    <span id="screamer_vr"></span>
+                    <a id="chatter_teeth_vr" href="vr" target="_blank" title="Launch Wahoo VR experience"></a>
+                </div>
+            </article>
+
+            <article>
+                <h2>Scan and compare the font</h2>
+                <header>
+                    <h3>Original and official logo</h3><br />
+                    <img src="img/vs-badge.png" alt="VS"/><br />
+                    <h3>Logo recreated with the font</h3><br />
+                    <span>0%</span>
+                </header>
+                <div id="comparator">
+                    <div><span><img src="img/btn_drag.png" alt="Btn drag"/></span><img src="img/Watch_Dogs_logo_original.png" alt="Original Watch Dogs Logo"/></div>
+                    <div><span><img src="img/btn_drag.png" alt="Btn drag"/></span><img src="img/Watch_Dogs_logo_font.png" alt="Recreated Watch Dogs Logo with the font"/></div>
+                </div>
+            </article>
+
+            <article>
+                <h2>Discover uses of the font</h2>
+                <div>
+                    <div>
+                        <div>
+                            <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Highly anticipated feature. We&#39;re glad you noticed. <a href="https://t.co/4zCeidVd5z" target="_blank">https://t.co/4zCeidVd5z</a></p>&mdash; Watch Dogs (@watchdogsgame) <a href="https://twitter.com/watchdogsgame/status/740609139215376384" target="_blank">8 juin 2016</a></blockquote>
+                        </div>
+                        <div>
+                            <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">You can pet dogs in WATCH_DOGS 2<br><br>I am now calling the game PET_DOGS <a href="https://t.co/gOZNTluSQV" target="_blank">pic.twitter.com/gOZNTluSQV</a></p>&mdash; Luis Illingworth (@HeyRaguio) <a href="https://twitter.com/HeyRaguio/status/740580479829704704" target="_blank">8 juin 2016</a></blockquote>
+                        </div>
+                        <div>
+                            <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">We have a winner <a href="https://twitter.com/hashtag/washdogs2?src=hash" target="_blank">#washdogs2</a> <a href="https://twitter.com/hashtag/watchdogs2?src=hash" target="_blank">#watchdogs2</a> <a href="https://t.co/DtqwfZ5Q94" target="_blank">https://t.co/DtqwfZ5Q94</a></p>&mdash; Watch Dogs (@watchdogsgame) <a href="https://twitter.com/watchdogsgame/status/740979664148140032" target="_blank">9 juin 2016</a></blockquote>
+                        </div>
+                        <div>
+                            <blockquote class="twitter-tweet" data-lang="en"><p lang="fr" dir="ltr"><a href="https://twitter.com/hashtag/Dedseccreations?src=hash" target="_blank">#Dedseccreations</a> <a href="https://twitter.com/hashtag/WatchDogs2?src=hash" target="_blank">#WatchDogs2</a> R3qu3st3d by <a href="https://twitter.com/AssassinWill529" target="_blank">@AssassinWill529</a> c: <a href="https://t.co/Qw1zfynUnp" target="_blank">pic.twitter.com/Qw1zfynUnp</a></p>&mdash; R@y W0lf0rd (@Ray_Wolford) <a href="https://twitter.com/Ray_Wolford/status/760213299195707393" target="_blank">1 août 2016</a></blockquote>
+                        </div>
+                    </div>
+                    <div>
+                        <a href="http://www.ign.com/articles/2014/04/23/watch-dogs-how-we-spent-our-first-5-hours" target="_blank">
+                            <header style="background-image:url(http://assets1.ignimgs.com/vid/thumbnails/user/2014/04/10/WatchDogs_TellingAStory_1280_large.jpg)">
+                                <h3>IGN article</h3>
+                            </header>
+                            <footer>
+                                http://www.ign.com/articles/2014/04/23/watch-dogs-how-we-spent-our-first-5-hours
+                            </footer>
+                        </a>
+                        
+                        <!--
+                        <a href="" target="_blank">
+                            <header style="background-image:url()">
+                                <h3></h3>
+                            </header>
+                            <footer>
+                                
+                            </footer>
+                        </a>
+                        -->
+                        
+                        <a href="https://www.youtube.com/watch?v=5ipUwUcHASI" target="_blank">
+                            <header style="background-image:url(img/reuses/ubisoft_ubiblog.png)">
+                                <h3>Ubisoft US video</h3>
+                            </header>
+                            <footer>
+                                https://www.youtube.com/watch?v=5ipUwUcHASI
+                            </footer>
+                        </a>
+                        <a href="https://www.youtube.com/watch?v=5Tt1QnkOw44" target="_blank">
+                            <header style="background-image:url(img/reuses/screen_video_squeezie_watchdogs.png)">
+                                <h3>SQUEEZIE video</h3>
+                            </header>
+                            <footer>
+                                https://www.youtube.com/watch?v=5Tt1QnkOw44
+                            </footer>
+                        </a>
+                        <a href="https://www.youtube.com/watch?v=09KF1ymlWqc" target="_blank">
+                            <header style="background-image:url(https://i.ytimg.com/vi/09KF1ymlWqc/hqdefault.jpg?custom=true&w=1960&h=1100&stc=true&jpg444=true&jpgq=90&sp=68&sigh=qAECqFDsqSxAkEWbXKfSco2R2nE)">
+                                <h3>Aypierre video</h3>
+                            </header>
+                            <footer>
+                                https://www.youtube.com/watch?v=09KF1ymlWqc
+                            </footer>
+                        </a>
+                        <a href="http://www.deezer.com/album/13571397" target="_blank">
+                            <header style="background-image:url(http://e-cdn-images.deezer.com/images/cover/476bf31143db59dacd427173efae8425/200x200-000000-80-0-0.jpg)">
+                                <h3>LVEEM music album</h3>
+                            </header>
+                            <footer>
+                                http://www.deezer.com/album/13571397
+                            </footer>
+                        </a>
+                        <a href="http://graphicdesignjunction.com/2015/04/super-free-fonts-download/" target="_blank">
+                            <header style="background-image:url(http://graphicdesignjunction.com/wp-content/uploads/2015/04/Hacked_free_font.jpg)">
+                                <h3>Design website</h3>
+                            </header>
+                            <footer>
+                                http://graphicdesignjunction.com/2015/04/super-free-fonts-download/
+                            </footer>
+                        </a>
+                        
+                        <a href="http://www.dafont.com/forum/requests.php?f=hacked" target="_blank">
+                            <header style="background-image:url(img/reuses/dafont_forum.png)">
+                                <h3>...and many more on Dafont forum</h3>
+                            </header>
+                            <footer>
+                                http://www.dafont.com/forum/requests.php?f=hacked
+                            </footer>
+                        </a>
+                    </div>
+                </div>
+                
+                
+            </article>
+
+            <article>
+                <h2>Download the font</h2>
+                <div id="download"><button><strong>Download</strong></button></div>
+                <p><strong>&gt;1.000.000+ followers&lt;</strong>
+                <img src="img/DEDSEC_HAND_04.png" alt="DEDSEC HAND"/> 
+                </p>
+            </article>
+
+            <article>
+                <h2>Create with the font</h2>
+                <p>
+                    The font is published under the <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">Creative commons Attribution (CC-BY) licence<img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>. So YOU CAN use the font for all your creations (even for commercial creations) only if you put the link "<strong>https://watchdogsfont.com</strong>" OR "<strong>https://hackedfont.com</strong>" OR the font creator's name "<strong>David Libeau</strong>" in your credits (YouTube video description, website About page...). So simple ! :)
+                </p>
+                <p>Also, important : keep in mind that this font is NOT an official font, but a remake from the Watch Dogs logo.<br/><a href="http://watchdogs.ubisoft.com/" target="_blank">Watch Dogs</a> is a game owned by <a href="https://www.ubisoft.com" target="_blank">Ubisoft</a>, <a href="http://watchdogs.ubisoft.com/" target="_blank">play it</a>!</p>
+            </article>
+
+            <article>
+                <h2>Share the font</h2>
+                <p><a href="https://twitter.com/intent/tweet?hashtags=WatchDogs&original_referer=https%3A%2F%2F
